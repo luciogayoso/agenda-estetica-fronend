d4cecd5953d3f548fb4c64ff8e48e055f008759d
vitaliamiranda <vitaliamiranda@gmail.com>
2026-08-19 10:05:14 -0300

    inicio de session de google

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from './App.jsx';
 import './index.css';
+
+// Obtiene el Client ID de las variables de entorno de Vite
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
